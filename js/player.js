e63602f22b3ae73614bf52f4590efc452c86064a
badboy <616860597@qq.com>
2017-06-07 11:45:58 +0800

    fir6

--- js/player.js
+++ js/player.js
@@ -3,13 +3,13 @@
 
 
 
-    var audio = document.getElementById('music');
-    bf();
+
+
     $('.audioPhoto').click(function () {
         $(this).toggleClass('audioplay');
 
+        audioAutoPlay('music');
 
-        bf();
     });
 
     $('.share').animate({'top':0},5000);
@@ -20,18 +20,13 @@
     },8000);
 
 
-    function bf(){
-
-        if(audio!==null){
-            //检测播放是否已暂停.audio.paused 在播放器播放时返回false.
-
-            if(audio.paused)                     {
-                audio.play();//audio.play();// 这个就是播放
-            }else{
-                audio.pause();// 这个就是暂停
-            }
-        }
+    function audioAutoPlay(id){
+        var audio = document.getElementById(id);
+        audio.play();
+        document.addEventListener("WeixinJSBridgeReady", function () {
+            audio.play();
+        }, false);
     }
-
+    audioAutoPlay('music');
 
 
